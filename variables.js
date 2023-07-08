9b28c415318c72235ce847969d7ccdc73b4b7778
kieswetter-hub <50299190+kieswetter-hub@users.noreply.github.com>
2023-07-08 09:17:49 +0200

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,19 @@
+/* The following script contains my notes and practice scripts 
+The Odin Projects Javascript fundamentals */
+
+/* Fundamentals Part 1 */
+
+console.log(23 + 97)
+console.log(1 + 6 + 8 + 7 + 4 +2)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
+
+let max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
